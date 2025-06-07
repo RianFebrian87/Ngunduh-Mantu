89a2bf087eaf7746d7a7c0e56173f2aef0fd42ce
RianFebrian87 <rianfebrian607@gmail.com>
2025-06-07 14:02:51 +0700

    mengubah jenis undangan

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,7 +69,7 @@ function toggleMusic(event)
 }
 
 // date counter
-var countDownDate = new Date("June 22, 2025 09:00:00").getTime()
+var countDownDate = new Date("June 29, 2025 11:30:00").getTime()
 
 var x = setInterval(function() {
 	var now = new Date().getTime()
